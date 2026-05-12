8eab2ba5dbc076f13b0bb79cec172f158e22c0a2
sba25150-afk <sba25150@student.cct.ie>
2026-05-12 19:02:31 +0100

    Update Uber.py

--- Uber.py
+++ Uber.py
@@ -22,3 +22,7 @@ data_load_state.text("Loading Data Done!")
 
 st.subheader("Raw Data")
 st.write(data)
+
+st.subheader("Num Pickups per hour")
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
