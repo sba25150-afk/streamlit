c8721ccb5421bee2afe257e39b41da8ad3a18539
sba25150-afk <sba25150@student.cct.ie>
2026-05-12 18:56:47 +0100

    Update Uber.py

--- Uber.py
+++ Uber.py
@@ -13,7 +13,7 @@ def load_data(nrows):
 	data=pd.read_csv(DATA_URL, nrows=nrows)
 	lowercase=lambda x: str(x).lower()
 	data.rename(lowercase, axis="columns",inplace=True)
-	data[DATE_COLUMN]#pd.to_datetime(data[DATE_COLUMN])
+	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
 
 data_load_state=st.text("Loading Data...")
